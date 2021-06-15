33f3aa39662602cba77dd6433ff56978d3ec933e
Ranjini <ranj.jini@gmail.com>
2021-06-15 13:27:55 +0100

    Added a new component ReadingList

---
+++ src/Components/ReadingList/ReadingList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppBar from "../AppBar/AppBar";
+// import Book from "../Book/Book";
+
+function readingList(){ 
+    return (
+        <section>
+           <h2>Title    Author</h2>
+           <ol>
+               {/* <Book/> */}
+           </ol>
+        </section>
+    )
+}
+
+export default readingList;
